6faaa7b2910529c69f153f3e13c862ef2541599a
Juan Manuel Del Castillo Navarro <delcastillonavarro@outlook.es>
2024-06-07 22:35:51 +0200

    Rebuild

--- src/main/java/com/example/aloha/config/WebConfig.java
+++ src/main/java/com/example/aloha/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://aloharg.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
